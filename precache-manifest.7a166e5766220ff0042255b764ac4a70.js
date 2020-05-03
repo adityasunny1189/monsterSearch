4e1448fb8b5e1a87354582d40989bf425959f2ab
adityasunny1189 <adityapathak1189@gmail.com>
2020-05-03 23:12:10 +0530

    Updates

---
+++ precache-manifest.7a166e5766220ff0042255b764ac4a70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5f0117e47c41f5a46b95b73cdfcad00",
+    "url": "/monsterSearch/index.html"
+  },
+  {
+    "revision": "d18b94b1adb21524bf9e",
+    "url": "/monsterSearch/static/css/main.3d031e51.chunk.css"
+  },
+  {
+    "revision": "4530896f27cbdb2cca16",
+    "url": "/monsterSearch/static/js/2.73d24dec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsterSearch/static/js/2.73d24dec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d18b94b1adb21524bf9e",
+    "url": "/monsterSearch/static/js/main.50fa8d74.chunk.js"
+  },
+  {
+    "revision": "c5d8aa83ac8364b66aed",
+    "url": "/monsterSearch/static/js/runtime-main.6bf27656.js"
+  }
+]);
